2d0191f1bd955219cfaa3ae712eb31a44b609cc3
ShenpaiSharma <shubhamstyles90113@gmail.com>
2022-09-08 13:16:06 +0530

    First Commit

---
+++ sketches/sketch1.js
@@ -0,0 +1,13 @@
+function setup() {
+    // Create the canvas
+    createCanvas(720, 400, WEBGL);
+    background(200);
+    
+    // Set colors
+    fill(204, 101, 192, 127);
+    stroke(127, 63, 120);
+    
+    // A rectangle
+    rect(-100, -100, 200, 80, 40, 1, 5, 1);
+   }
+   
